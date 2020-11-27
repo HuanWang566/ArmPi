8496b8cef7d01413f1903f0c797df6ab3a85d71a
huanwang566 <584390218@qq.com>
2020-11-27 13:49:19 +0800

    ajust parameters

--- Functions/ColorSorting.py
+++ Functions/ColorSorting.py
@@ -181,7 +181,7 @@ def move():
                     
                     if not __isRunning:
                         continue
-                    AK.setPitchRangeMoving((world_X, world_Y, 4), -90, -90, 0, 1000)
+                    AK.setPitchRangeMoving((world_X, world_Y, 3), -90, -90, 0, 1000)
                     time.sleep(1.5)
 
                     if not __isRunning:
