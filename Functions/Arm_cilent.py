430cc53d0fd4594c37aa4dd954737a851ddc3532
huanwang566 <584390218@qq.com>
2020-12-03 20:59:44 +0800

    continuing setting status until success

--- Functions/Arm_cilent.py
+++ Functions/Arm_cilent.py
@@ -4,12 +4,13 @@ import numpy as np
 server = xmlrpc.client.ServerProxy("http://192.168.10.71:50051", allow_none=True)
 
 def get_car_pos(arm_id, car_id):
-    car_pos = server.get_car_postion(car_id)
-    return car_pos
+    return server.get_car_postion(car_id)
 
 def set_arm_status(arm_id, status):
-    server.set_arm_status(arm_id, status)
+    while True:
+        server.set_arm_status(arm_id, status)
+        if get_arm_status(arm_id) == status:
+            break
 
 def get_arm_status(arm_id):
-    arm_status = server.get_arm_status(arm_id)
-    return arm_status
+    return server.get_arm_status(arm_id)
