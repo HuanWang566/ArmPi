7c5e92d9bd0bc21855f5d91210c8894acda25e30
huanwang566 <584390218@qq.com>
2020-12-15 01:09:33 +0800

    ajust parameters

--- Functions/ColorSorting.py
+++ Functions/ColorSorting.py
@@ -351,11 +351,11 @@ def run(img, img_idx):
             world_x, world_y = convertCoordinate(img_centerx, img_centery, size) #转换为现实世界坐标
 
             if img_idx == 1:
-                world_x -= 21.5
-                world_y -= 25
-            elif img_idx == 0:
-                world_x += 24
+                world_x -= 24
                 world_y -= 20
+            elif img_idx == 0:
+                world_x += 25
+                world_y -= 17.5
             
             cv2.drawContours(img, [box], -1, range_rgb[color_area_max], 2)
             cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
